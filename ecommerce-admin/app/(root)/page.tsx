5030bece90c43c16d753223a826af2e817c18261
Jace L. Gonzales <jacegonzales25@gmail.com>
2023-09-09 20:13:54 +0800

    Added Clerk Authentication, using documentation

--- ecommerce-admin/app/(root)/page.tsx
+++ ecommerce-admin/app/(root)/page.tsx
@@ -1,12 +1,9 @@
-import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs"
 
 export default function Home() {
   return (
     <div className="p-4">
-      <p>Hello Admin Dashboard</p>
-      <Button>Click Me</Button>  
+      <UserButton afterSignOutUrl="/auth/sign-in" />
     </div>
-    
-    
   )
 }
